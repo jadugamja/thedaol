16e23d7d8b2e28891a16a23c4d52882b44e153d0
jadugamja <gheddong2@naver.com>
2026-01-19 16:38:31 +0900

    move: 상세 페이지 위치 이동

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "standalone",
   images: {
     unoptimized: true,
   },
+  basePath: "",
   compress: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
