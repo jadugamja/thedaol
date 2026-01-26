5a1d0b816055e81ca6513d403f6c4f81dcaaae6c
jadugamja <gheddong2@naver.com>
2026-01-26 13:39:00 +0900

    chore: window.naver -> naver

--- components/utils/NaverMap.tsx
+++ components/utils/NaverMap.tsx
@@ -9,8 +9,7 @@ declare global {
 }
 
 export default function NaverMap() {
-  const mapRef = useRef<HTMLDivElement>(null);
-  const mapInstanceRef = useRef<naver.maps.Map>(null);
+  const mapRef = useRef<HTMLDivElement>(null); // DOM
 
   useEffect(() => {
     // 1. 네이버 지도 스크립트 동적 로드
@@ -19,9 +18,10 @@ export default function NaverMap() {
     script.async = true;
 
     script.onload = () => {
-      if (mapRef.current && window.naver) {
+      const { naver } = window;
+      if (mapRef.current && naver) {
         // 2. 지도 생성
-        const location = new window.naver.maps.LatLng(
+        const location = new naver.maps.LatLng(
           37.4835033620443, // 위도
           126.881038151818, // 경도
         );
@@ -31,22 +31,21 @@ export default function NaverMap() {
           zoom: 16,
           zoomControl: true,
           zoomControlOptions: {
-            position: window.naver.maps.Position.TOP_RIGHT,
+            position: naver.maps.Position.TOP_RIGHT,
           },
         };
 
-        const map = new window.naver.maps.Map(mapRef.current, mapOptions);
-        mapInstanceRef.current = map;
+        const map = new naver.maps.Map(mapRef.current, mapOptions);
 
         // 3. 마커
-        const marker = new window.naver.maps.Marker({
+        const marker = new naver.maps.Marker({
           position: location,
           map: map,
           title: "더다올디앤씨",
         });
 
-        // 4. 정보창 추가
-        const infowindow = new window.naver.maps.InfoWindow({
+        // 4. 정보창
+        const infowindow = new naver.maps.InfoWindow({
           content: `
             <div style="padding: 12px;">
               <h3 style="font-size: 16px; font-weight: bold;">더다올디앤씨</h3>
@@ -54,8 +53,8 @@ export default function NaverMap() {
           `,
         });
 
-        // 5. 마커 클릭 이벤트
-        window.naver.maps.Event.addListener(marker, "click", () => {
+        // 5. 마커 클릭 이벤트 -> 정보창 열림
+        naver.maps.Event.addListener(marker, "click", () => {
           if (infowindow.getMap()) {
             infowindow.close();
           } else {
@@ -68,6 +67,7 @@ export default function NaverMap() {
     document.head.appendChild(script);
 
     return () => {
+      // 컴포넌트 언마운트 시 스크립트 제거
       if (script.parentNode) {
         script.parentNode.removeChild(script);
       }
